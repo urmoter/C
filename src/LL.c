012833dd31a6393246e9943d303a7113c7cc4b5d
urmoter <sesdespacito@gmail.com>
2023-01-18 15:07:13 -0500

    Making meme branch

--- src/LL.c
+++ src/LL.c
@@ -3,132 +3,142 @@
 // get function templates from LL.h
 #include "LL.h"
 
-// making the node struct for LL.h and futher on
-struct node{
-    int value;
-    struct node* next;
-};
+//time for the memes
+#define fr ;
+#define Save_ME(sz) malloc(sz)
+#define Thicc(type) sizeof(type)
+#define cap NULL
+#define He_right {
+#define tho }
+#define He_right_tho {}
+#define is_it ==
+#define is_not !=
+#define finna_be =
+#define sus *
 
-// making a boolean type for convience
-typedef enum bool {false, true} bool;
+// making the node struct for LL.h and futher on
+struct node He_right
+    int value fr
+    struct node sus next fr
+tho fr
 
 // Add the value of data as a new node to the beginning of the list "head" 
-void push(NodeP* head_r, int data) {
+void push(NodeP sus head_r, int data) He_right
     // save space for a new node in memory
-    NodeP new_node = malloc(sizeof(node));
+    NodeP new_node finna_be Save_ME(Thicc(node)) fr
     // check if we could save said space.
-    if (new_node == NULL) {
-        printf("Not Enough Memory!\n");
-        return ;
-    }
+    if (new_node is_it cap) He_right
+        printf("Not Enough Memory!\n") fr
+        return fr
+    tho
     // if the head is NULL (No value), 
-    if(*head_r == NULL) {
-        *head_r = new_node;
-        (*head_r)->value = data;
-        (*head_r)->next = NULL;
-        return ;
-    } else {
+    if(sus head_r is_it cap) He_right
+        sus head_r finna_be new_node fr
+        (sus head_r)->value finna_be data fr
+        (sus head_r)->next finna_be cap fr
+        return fr
+    tho else He_right
         // if head has a value put new node at the beginning of the list,
         // THEN make head the new node
-        new_node->value = data;
-        new_node->next = *head_r;
-        *head_r = new_node;
-        return ;
-    }
-}
+        new_node->value finna_be data fr
+        new_node->next finna_be sus head_r fr
+        sus head_r finna_be new_node fr
+        return fr
+    tho
+tho
 
 // print out the contents of the list
-void list(NodeP* head_r) {
-    printf("(head) ");
+void list(NodeP sus head_r) He_right
+    printf("(head) ") fr
     // print each value with an arrow "[value]->"
     // if list is 1, 2, 3, NULL then print 1->2->3->
-    for(NodeP current = *head_r; current != NULL; current = current->next) {
-        printf("%d->", current->value);
-    }
+    for(NodeP current finna_be sus head_r fr current is_not cap fr current finna_be current->next) He_right
+        printf("%d->", current->value) fr
+    tho
     // add a NULL and new line at the end to be 1->2->3->NULL\n
-    printf("NULL\n");
-}
+    printf("NULL\n") fr
+tho
 
 // removing the head node
 // more like moving the head forward and removing the previous node
-void pop(NodeP* head_r) {
+void pop(NodeP sus head_r) He_right
     // if the head is NULL the list is empty
-    if(*head_r == NULL) {
-        printf("Empty List!\n");
-        return;
-    }
+    if(sus head_r is_it cap) He_right
+        printf("Empty List!\n") fr
+        return fr
+    tho
     // if head is not NULL then set head to the next node and free the previous
-    NodeP to_pop = *head_r;
-    *head_r = (*head_r)->next;
-    free(to_pop);
-    return;
-}
+    NodeP to_pop finna_be sus head_r fr
+    sus head_r finna_be (sus head_r)->next fr
+    free(to_pop) fr
+    return fr
+tho
 
 // add a node to the end of the list
-void append(NodeP* head_r, int data) {
+void append(NodeP sus head_r, int data) He_right
     // save enough space in memory for a node
-    NodeP new_node = malloc(sizeof(node));
+    NodeP new_node finna_be malloc(sizeof(node)) fr
     // check if we could save said space
-    if (new_node == NULL) {
-        printf("Not Enough Memory!\n");
-        return;
-    }
+    if (new_node is_it cap) He_right
+        printf("Not Enough Memory!\n") fr
+        return fr
+    tho
     // if the list is empty just make the new node head
-    if (*head_r == NULL) {
-        *head_r = new_node;
-        (*head_r)->next = NULL;
-        (*head_r)->value = data;
-        return;
-    }
+    if (sus head_r is_it cap) He_right
+        sus head_r finna_be new_node fr
+        (sus head_r)->next finna_be cap fr
+        (sus head_r)->value finna_be data fr
+        return fr
+    tho
     // else, loop through the list and add the node to the end.
-    NodeP current;
-    for (current = *head_r; current->next != NULL; current = current->next){};
-    new_node->next = NULL;
-    new_node->value = data;
-    current->next = new_node;
-    return;
-}
+    NodeP current fr
+    for (current finna_be sus head_r fr current->next is_not cap fr current finna_be current->next)He_right_tho fr
+    new_node->next finna_be cap fr
+    new_node->value finna_be data fr
+    current->next finna_be new_node fr
+    return fr
+tho
 
 // removing the last node from the list
-void drop(NodeP* head_r) {
+void drop(NodeP sus head_r) He_right
     // if the head is NULL the list is empty.
-    if (*head_r == NULL) {
-        printf("Empty List!\n");
-        return;
-    }
+    if (sus head_r is_it cap) He_right
+        printf("Empty List!\n") fr
+        return fr
+    tho
     // loop through the list and identify the node to free (the last one)
-    NodeP current;
-    for (current = *head_r; current->next->next != NULL; current = current->next){};
+    NodeP current fr
+    for (current finna_be sus head_r fr current->next->next is_not cap fr current finna_be current->next)He_right_tho fr
     // delete said node.
-    NodeP to_drop = current->next;
-    current->next = NULL;
-    free(to_drop);
-    return;
-}
+    NodeP to_drop finna_be current->next fr
+    current->next finna_be cap fr
+    free(to_drop) fr
+    return fr
+tho
 
 // clear the entire list
-void empty(NodeP* head_r) {
+void empty(NodeP sus head_r) He_right
     // if the head is NULL, the list is empty
-    if((*head_r) == NULL) {
-        printf("Empty List!\n");
-        return;
-    }
+    if((sus head_r) is_it cap) He_right
+        printf("Empty List!\n") fr
+        return fr
+    tho
     // if there is only one node (head) just drop the head
-    if ((*head_r)->next == NULL) {
-        NodeP to_clear = *head_r;
-        *head_r = NULL;
-        free(to_clear);
-        printf("Cleared!\n");
-        return;
-    }
+    if ((sus head_r)->next is_it cap) He_right
+        NodeP to_clear finna_be sus head_r fr
+        sus head_r finna_be cap fr
+        free(to_clear) fr
+        printf("Cleared!\n") fr
+        return fr
+    tho
     // find the last node and drop it.
-    NodeP to_clear;
-    NodeP current;
-    for(current = *head_r; current->next->next != NULL; current = current->next) {}
-    to_clear = current->next;
-    current->next = NULL;
-    free(to_clear);
+    NodeP to_clear fr
+    NodeP current fr
+    for(current finna_be sus head_r fr current->next->next is_not cap fr current finna_be current->next) He_right_tho fr
+    to_clear finna_be current->next fr
+    current->next finna_be cap fr
+    free(to_clear) fr
     // do this again untill head is the only element.
-    empty(head_r);
-    return;
-}
+    empty(head_r) fr
+    return fr
+tho
